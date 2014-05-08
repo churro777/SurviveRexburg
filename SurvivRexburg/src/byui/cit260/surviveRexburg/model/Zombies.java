16abfbfb080016b7ce893a6d011ab964926064ae
Arturo Aguila <agu09001@byui.edu>
2014-05-08 17:45:13 -0600

    adding Zombie class

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/model/Zombies.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author arturoaguila
+ */
+public class Zombies implements Serializable{
+    
+    //class instance variables
+    private double numberOfZombies
+
+    // getter and setter functions
+    public double getNumberOfZombies() {
+        return numberOfZombies;
+    }
+
+    public void setNumberOfZombies(double numberOfZombies) {
+        this.numberOfZombies = numberOfZombies;
+    }
+    
+    //other functions
+    
+    
+    
+    
+    
+}
