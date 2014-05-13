a04533d55e7d4f9a00dfee49309328b75e6da228
carissa888 <ada12025@byui.edu>
2014-05-13 15:40:57 -0600

    End user class completed

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/model/EndUser.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.model;
+
+import java.io.Serializable;
+        
+/**
+ *
+ * @author carissa888
+ */
+public class EndUser implements Serializable{
+    
+    //attributes
+    private int bestScore;
+    
+    //constructor
+    public EndUser() {
+    }
+    
+    
+    //getter and setter
+    public int getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(int bestScore) {
+        this.bestScore = bestScore;
+           
+    }
+    
+    //other funtions
+    private boolean doNothing(){
+        return true || false;
+    }
+    
+    private boolean fortify(){
+        return true || false;
+    }
+    
+    private boolean explore(){
+        return true || false;
+    }
+    
+    private boolean fight(){
+        return true || false;
+    }
+    
+    private boolean negotiate(){
+        return true || false;
+    }
+    
+    private boolean run(){
+        return true || false;
+    }
+    
+    private boolean beg(){
+        return true || false;
+    }
+    
+    private boolean getCaught(){
+        return true || false;
+    }
+    
+    private boolean escape(){
+        return true || false;
+    }
+    
+    private boolean getRobbed(){
+        return true || false;
+    }
+    
+    private boolean getInjured(){
+        return true || false;
+    }
+    
+    private boolean die(){
+        return true || false;
+    }
+    
+     private boolean winGame(){
+        return true || false;
+    }
+    
+    private boolean starve(){
+        return true || false;
+    }
+    
+  
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.bestScore;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EndUser other = (EndUser) obj;
+        if (this.bestScore != other.bestScore) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EndUser{" + "bestScore=" + bestScore + '}';
+    }
+       
+}
