695104f8ccf1e2438debe1063cc178b6fbe55223
carissa888 <ada12025@byui.edu>
2014-05-13 15:42:23 -0600

    Still working on MeleeWeapons class. Finished the contructor, still need to add in equals and hashCode and instance

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/model/MeleeWeapons.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author carissa888
+ */
+public class MeleeWeapons implements Serializable{
+    
+    //attributes
+    private int strengthValue;
+    
+    //getter and setter
+    public int getStrengthValue() {
+        return strengthValue;
+    }
+
+    public void setStrengthValue(int strengthValue) {
+        this.strengthValue = strengthValue;
+    }
+    
+    //constructor
+    public MeleeWeapons() {
+    }
+    
+    //toString
+    
+    
+    
+}
