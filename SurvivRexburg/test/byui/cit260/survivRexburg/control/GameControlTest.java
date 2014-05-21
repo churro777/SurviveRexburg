caec8fb79064f909c91d1ba6d5c7549a0c05b167
Arturo Aguila <agu09001@byui.edu>
2014-05-21 16:04:15 -0600

    GameControlTest done

---
+++ SurvivRexburg/test/byui/cit260/survivRexburg/control/GameControlTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.survivRexburg.control;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author arturoaguila
+ */
+public class GameControlTest {
+    
+    public GameControlTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of increaseDailyDifficulty method, of class GameControl.
+     */
+    @Test
+    public void testIncreaseDailyDifficulty() {
+        System.out.println("increaseDailyDifficulty");
+        int scenarioValue = 54;
+        int daysPassed = 10;
+        GameControl instance = new GameControl();
+        int expResult = 44;
+        int result = instance.increaseDailyDifficulty(scenarioValue, daysPassed);
+        assertEquals(expResult, result);
+    }
+    
+}
