489b64e8f823851a8898c0542688554a83381e45
carissa888 <ada12025@byui.edu>
2014-05-29 18:03:46 -0600

    MainMenuView

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/MainMenuView.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.view;
+
+/**
+ *
+ * @author carissa888
+ */
+public class MainMenuView {
+
+    void displayMenu() {
+        // @todo
+        System.out.println("\n***** MainMenu displayMenu function called ****");
+    }
+    
+}
