aaa7faa953687e4e959ae77e5e32eefd9425973d
Arturo Aguila <agu09001@byui.edu>
2014-06-01 15:18:59 -0600

    StartProgramViewTest commmit

---
+++ SurvivRexburg/test/byui/cit260/surviveRexburg/view/StartProgramViewTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.view;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author arturoaguila
+ */
+public class StartProgramViewTest {
+    
+    public StartProgramViewTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of startProgram method, of class StartProgramView.
+     */
+    @Test
+    public void testStartProgram() {
+        System.out.println("startProgram");
+        StartProgramView instance = new StartProgramView();
+        instance.startProgram();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayBanner method, of class StartProgramView.
+     */
+    @Test
+    public void testDisplayBanner() {
+        System.out.println("displayBanner");
+        StartProgramView instance = new StartProgramView();
+        instance.displayBanner();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
