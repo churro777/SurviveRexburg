9d064163134849d03fe797eb6136a064d005d35f
Arturo Aguila <agu09001@byui.edu>
2014-06-01 15:17:16 -0600

    Survive rexburg commit

--- SurvivRexburg/src/Survivrexburg/SurviveRexburg.java
+++ SurvivRexburg/src/Survivrexburg/SurviveRexburg.java
@@ -21,6 +21,7 @@ import byui.cit260.surviveRexburg.model.ScenarioLocation;
 import byui.cit260.surviveRexburg.model.SpoiledFood;
 import byui.cit260.surviveRexburg.model.Survivors;
 import byui.cit260.surviveRexburg.model.Zombies;
+import byui.cit260.surviveRexburg.view.StartProgramView;
 
 
 /**
@@ -54,146 +55,11 @@ public class SurviveRexburg {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //instance of a game
-        Game game = new Game();
-        
-        game.setDaysPassed(20);
-        game.setSavedGameName("Arty");
-
-        String gameInfo = game.toString();
-        System.out.println(gameInfo);
-    
-        //instance of a GameCharacter - Ben Brown
-        GameCharacter ben = new GameCharacter();
-        
-        ben.setCharacterName("Ben Brown");
-        ben.setcharCharismaValue(1);
-        ben.setcharHealthValue(100);
-        ben.setcharHungerValue(100);
-        ben.setcharLuckValue(4);
-        ben.setcharSpeedValue(5);
-        ben.setcharStrengthValue(10);
-        
-        String benInfo = ben.toString();
-        System.out.println(benInfo);
-         
-        //instance of Zombie - nathan
-        Zombies nathan = new Zombies();
-        
-        nathan.setNumberOfZombies(1);
-        nathan.setzombieStrengthValue(10);
-        
-        String nathanInfo = nathan.toString();
-        System.out.println(nathanInfo);
-        
-        //instance of Location - Walmart
-        Location walmart = new Location();
-        
-        walmart.setName("Walmart");
-        walmart.setColumnNumber(1);
-        walmart.setRowNumber(7);
-        
-        String walmartInfo = walmart.toString();
-        System.out.println(walmartInfo);
-        
-        //instance of Map
-        Map rexburg = new Map();
-        
-        rexburg.setColumnCount(15);
-        rexburg.setRowCount(15);
-        
-        String rexburgInfo = rexburg.toString();
-        System.out.println(rexburgInfo);
-        
-        //instance of ScenarioLocation
-        ScenarioLocation fortify = new ScenarioLocation();
-        
-        fortify.setZombieKillRecord(14);
-        fortify.setVisited(false);
-        fortify.setSurvivorKillRecord(2);
-        fortify.setSurvivorHelpRecord(6);
-        fortify.setNotVisited(true);
-        fortify.setEffectsToCharacter(5.43356);
-        
-        String fortifyInfo = fortify.toString();
-        System.out.println(fortifyInfo);
-        
-        //instace of Scenario
-        Scenario dayEnds = new Scenario();
-        
-        dayEnds.setScenarioValue(100);
-        
-        String dayEndsInfo = dayEnds.toString();
-        System.out.println(dayEndsInfo);
-        
-        //instance for Survivors
-        Survivors jack = new Survivors();
-        
-        jack.setsurvivorStrengthValue(2);
-        
-        String jackInfo = jack.toString();
-        System.out.println(jackInfo);
-        
-        //instance of EndUser
-            EndUser kevin = new EndUser();
-            
-            kevin.setBestScore(56);
-            
-            String kevinInfo = kevin.toString();
-            System.out.println(kevinInfo);
-        
-        //instance of Backpack
-            Backpack backpackOne = new Backpack();
-            
-            backpackOne.setMaxWeight(70);
-            
-            String backpackOneInfo = backpackOne.toString();
-            System.out.println(backpackOneInfo);
-            
-        //instance of IventoryType
-            InventoryItems meleeWeapon = new InventoryItems();
-            
-            meleeWeapon.setWeight(5);
-            meleeWeapon.setInventoryType("melee");
-            meleeWeapon.setName("baseball bat");
-            
-            String meleeWeaponInfo = meleeWeapon.toString();
-            System.out.println(meleeWeaponInfo);
-        
-        //instance of MeleeWeapon
-            MeleeWeapons baseballBat = new MeleeWeapons();
-            
-            baseballBat.setmeleeWeaponStrengthValue(9);
-            
-            String baseballBatInfo = baseballBat.toString();
-            System.out.println(baseballBatInfo);
-            
-        //instance of RangedWeapon
-            RangedWeapons shotgun = new RangedWeapons();
-            
-            shotgun.setrangedWeaponLuckValue(10);
-            
-            String shotgunInfo = shotgun.toString();
-            System.out.println(shotgunInfo);
-            
-        //instance of Food
-            Food cannedTuna = new Food();
-            
-            cannedTuna.setfoodHungerValue(10);
-            cannedTuna.setfoodHealthValue(10);
-            
-            String cannedTunaInfo = cannedTuna.toString();
-            System.out.println(cannedTunaInfo);
-            
-        //instance of Spoiled Food
-            SpoiledFood rottenEggs = new SpoiledFood();
-            
-            rottenEggs.setspoiledFoodHungerValue(5);
-            rottenEggs.setspoiledFoodHealthValue(-10);
-            
-            String rottenEggsInfo = rottenEggs.toString();
-            System.out.println(rottenEggsInfo);
+        //create StartProgramView and start the program
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
     }
+    //end of class
 }
 
 
