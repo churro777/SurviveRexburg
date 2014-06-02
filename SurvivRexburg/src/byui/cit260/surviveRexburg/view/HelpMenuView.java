4ee604b8acc517b648cbe4679be2610cf809a9f3
carissa888 <ada12025@byui.edu>
2014-06-02 15:49:04 -0600

    I made the Help Menu look pretty

--- SurvivRexburg/src/byui/cit260/surviveRexburg/view/HelpMenuView.java
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/HelpMenuView.java
@@ -14,33 +14,33 @@ import java.util.Scanner;
  */
 class HelpMenuView {
     private final String HELPMENU = "\n"
-            + "\n---------------HELP MENU-----------------"
-            + "\nG - What is the goal of the game?"
-            + "\nM - How to move"
-            + "\nE - How to Equip an Item"
-            + "\nQ - Quit to Main Menu"
-            + "\n-----------------------------------------";
+            + "\n------------------HELP MENU--------------------"
+            + "\n|G - What is the goal of the game?            |"
+            + "\n|M - How to move                              |"
+            + "\n|E - How to Equip an Item                     |"
+            + "\n|Q - Quit to Main Menu                        |"
+            + "\n-----------------------------------------------";
 
     //attributes of the displayStuff functions
     private final String GOALDISPLAY  ="\n"
-            + "\n----------------------------------------------"
-            + "\n The goal of SURVIVE REXBURG is to survive"
-            + "\n for 30 days. You will need to fend of "
-            + "\n both the living and the dead. But your worst"
-            + "\n enemy will be starvation. Do your best to "
-            + "\n find food wherever you can and make sure to"
-            + "\n eat it wisely."
-            + "\n----------------------------------------------";
+            + "\n-----------------------------------------------"
+            + "\n| The goal of SURVIVE REXBURG is to survive   |"
+            + "\n| for 30 days. You will need to fend of       |"
+            + "\n| both the living and the dead. But your worst|"
+            + "\n| enemy will be starvation. Do your best to   |"
+            + "\n| find food wherever you can and make sure to |"
+            + "\n| eat it wisely.                              |"
+            + "\n-----------------------------------------------";
     private final String MOVEMENTDISPLAY ="\n"
-            + "\n----------------------------------------------"
-            + "\n WE'RE NOT SURE YET. Ask again later. Arrows"
-            + "\n or something."
-            + "\n ---------------------------------------------";
+            + "\n-----------------------------------------------"
+            + "\n| WE'RE NOT SURE YET. Ask again later. Arrows |"
+            + "\n| or something.                               |" 
+            + "\n-----------------------------------------------";
             
     private final String EQUIPMENTDISPLAY ="\n"
-            + "\n----------------------------------------------"
-            + "\n IDK man. Ask me later"
-            + "\n----------------------------------------------";
+            + "\n-----------------------------------------------"
+            + "\n| IDK man. Ask me later                       |"
+            + "\n-----------------------------------------------";
     
     public void displayMenu() {
         char selection = ' ';
