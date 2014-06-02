66ffc30ba71b26d2b714fe85fb2195f0187d2131
carissa888 <ada12025@byui.edu>
2014-06-02 15:48:51 -0600

    I made the Help Menu look pretty

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/ChooseCharacterView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.view;
+
+/**
+ *
+ * @author carissa888
+ */
+public class ChooseCharacterView {
+    
+    private final String MENU = "\n"
+            + "\n-------------CHOOSE YOUR CHARACTER------------"
+            + "\n|I - Bing                                    |"
+            + "\n|Strength: 6, Speed: 8, Charisma: 3, Luck: 3 |"
+            + "\n----------------------------------------------"
+            + "\n|B - Ben                                     |"
+            + "\n|Strength: 10, Speed: 5, Charisma: 1, Luck: 4|"
+            + "\n----------------------------------------------"
+            + "\n|M - Megan                                   |"
+            + "\n|Stength: 6, Speed: 2, Charisma: 8, Luck: 4  |"
+            + "\n----------------------------------------------"
+            + "\n|H - Hayley                                  |"
+            + "\n|Stength: 1, Speed: 5, Charisma: 6, Luck: 8  |"
+            + "\n----------------------------------------------"
+            + "\n"
+            + "\nType in the letter to choose your character:";
+    
+    
+    
+    
+    
+}
