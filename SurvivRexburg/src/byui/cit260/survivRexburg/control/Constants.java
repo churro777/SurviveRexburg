00cd8d399c1cf2b6edad93482221adb7afbd099d
Arturo Aguila <agu09001@byui.edu>
2014-06-04 16:26:43 -0600

    Constants updated

---
+++ SurvivRexburg/src/byui/cit260/survivRexburg/control/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.survivRexburg.control;
+
+/**
+ *
+ * @author arturoaguila
+ */
+public class Constants {
+    
+    private final static String BING = "Bing";
+    private final static String BEN = "Ben";
+    private final static String HAYLEY = "Hayley";
+    private final static String MEGAN = "Megan";
+    
+}
