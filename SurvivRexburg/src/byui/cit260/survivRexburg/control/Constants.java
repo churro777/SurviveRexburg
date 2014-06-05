50e56bd23fd712994d408c26588e518d6876e0c9
Arturo Aguila <agu09001@byui.edu>
2014-06-05 16:25:32 -0600

    Constants updated

--- SurvivRexburg/src/byui/cit260/survivRexburg/control/Constants.java
+++ SurvivRexburg/src/byui/cit260/survivRexburg/control/Constants.java
@@ -11,10 +11,37 @@ package byui.cit260.survivRexburg.control;
  * @author arturoaguila
  */
 public class Constants {
-    
+    //Characters and their stats
     private final static String BING = "Bing";
+    private final static int BING_ST = 6;
+    private final static int BING_SP = 8;
+    private final static int BING_CH = 3;
+    private final static int BING_LU = 3;
+    
     private final static String BEN = "Ben";
+    private final static int BEN_ST = 10;
+    private final static int BEN_SP = 5;
+    private final static int BEN_CH = 1;
+    private final static int BEN_LU = 4;
+    
     private final static String HAYLEY = "Hayley";
+    private final static int HAYLEY_ST = 1;
+    private final static int HAYLEY_SP = 5;
+    private final static int HAYLEY_CH = 6;
+    private final static int HAYLEY_LU = 8;
+    
     private final static String MEGAN = "Megan";
+    private final static int MEGAN_ST = 6;
+    private final static int MEGAN_SP = 2;
+    private final static int MEGAN_CH = 8;
+    private final static int MEGAN_LU = 4;
+    
+    
+    
+    
+    
+    //Map
+    public final static int MAP_ROW_COUNT = 8;
+    public final static int MAP_COLUMN_COUNT = 8;
     
 }
