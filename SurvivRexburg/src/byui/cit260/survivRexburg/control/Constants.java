4f5fb56d62d6318af93393a53b8857b20ae88dec
Arturo Aguila <agu09001@byui.edu>
2014-06-10 16:40:39 -0600

    Constants updated

--- SurvivRexburg/src/byui/cit260/survivRexburg/control/Constants.java
+++ SurvivRexburg/src/byui/cit260/survivRexburg/control/Constants.java
@@ -12,36 +12,114 @@ package byui.cit260.survivRexburg.control;
  */
 public class Constants {
     //Characters and their stats
-    private final static String BING = "Bing";
-    private final static int BING_ST = 6;
-    private final static int BING_SP = 8;
-    private final static int BING_CH = 3;
-    private final static int BING_LU = 3;
+    public final static int NUMBER_OF_CHARACTERS = 4;
     
-    private final static String BEN = "Ben";
-    private final static int BEN_ST = 10;
-    private final static int BEN_SP = 5;
-    private final static int BEN_CH = 1;
-    private final static int BEN_LU = 4;
+    public static final int BEN = 0;
     
-    private final static String HAYLEY = "Hayley";
-    private final static int HAYLEY_ST = 1;
-    private final static int HAYLEY_SP = 5;
-    private final static int HAYLEY_CH = 6;
-    private final static int HAYLEY_LU = 8;
+    public final static int BING = 1;
     
-    private final static String MEGAN = "Megan";
-    private final static int MEGAN_ST = 6;
-    private final static int MEGAN_SP = 2;
-    private final static int MEGAN_CH = 8;
-    private final static int MEGAN_LU = 4;
+    public final static int HAYLEY = 2;
     
-    
-    
-    
+    public final static int MEGAN = 3;
     
     //Map
     public final static int MAP_ROW_COUNT = 8;
     public final static int MAP_COLUMN_COUNT = 8;
     
+    
+    public final static int GUNSTORE = 0;
+    
+    public final static String BUNKHOUSE = "Bunkhouse";
+    public final static String ROYALCREST = "Royal Crest";
+    public final static String HARTBUILDING = "Bunkhouse";
+    public final static String MC = "MC";
+    public final static String TACOBELL = "Taco Bell";
+    public final static String BROULIMS = "Broulim's";
+    public final static String WENDYS = "Wendy's";
+    public final static String COLONIALHOUSE = "Colonial House";
+    public final static String SNOWBUILDING = "Bunkhouse";
+    public final static String MOUNTAINPINES = "Bunkhouse";
+    public final static String SHAKEOUT = "Bunkhouse";
+    public final static String BARONESSA = "Bunkhouse";
+    public final static String SUBWAY = "Bunkhouse";
+    public final static String BROOKLYN = "Bunkhouse";
+    public final static String SMITHBUILDING = "Bunkhouse";
+    public final static String RICKSBUILDING = "Bunkhouse";
+    public final static String TAYLORBUILDING = "Bunkhouse";
+    public final static String ICENTER = "Bunkhouse";
+    public final static String TACOBUS = "Bunkhouse";
+    public final static String TOWERS = "Bunkhouse";
+    public final static String SOUTHGATE = "Bunkhouse";
+    public final static String NORTHGATE = "Bunkhouse";
+    public final static String WILLOWS = "Bunkhouse";
+    public final static String LITTLECESARS = "Bunkhouse";
+    public final static String GRINGOS = "Bunkhouse";
+    public final static String JACKINTHEBOX = "Bunkhouse";
+    public final static String FATCATS = "Bunkhouse";
+    public final static String WALMART = "Bunkhouse";
+    public final static String BIGFIVE = "Bunkhouse";
+    public final static String ALBERTSONS = "Bunkhouse";
+    
+    //items
+    public final static int NUMBER_OF_ITEMS = 47;
+    
+    //food
+    public final static int CANNED_TUNA         = 0;
+    public final static int CANNED_BEANS        = 1;
+    public final static int MANGOS              = 2;
+    public final static int CANNED_CHICKEN      = 3;
+    public final static int CANED_BEEF_STEW     = 4;
+    public final static int APPLES              = 5;
+    public final static int BANANAS             = 6;
+    public final static int SALTINE_CRACKERS    = 7;
+    public final static int CANNED_CHICKEN_SOUP = 8;
+    public final static int MILK                = 9;
+    public final static int POTATOES            = 10;
+    public final static int BREAD               = 11;
+    public final static int CHEESE              = 12;
+    public final static int CEREAL              = 13;
+    public final static int EGGS                = 14;
+    public final static int TOP_RAMEN           = 15;
+    public final static int CHOCOLATE           = 16;
+    public final static int SODA                = 17;
+    
+    //spoiled food
+    public final static int ROTTEN_EGGS     = 18;
+    public final static int MOLDY_CHEESE    = 19;
+    public final static int SOUR_MILK       = 20;
+    public final static int MOLDY_BREAD     = 21;
+    public final static int ROTTEN_MANGO    = 22;
+    public final static int ROTTEN_APPLE    = 23;
+    public final static int ROTTEN_BANANA   = 24;
+    
+    //melee weapons
+    public final static int BASEBALL_BAT    = 25;
+    public final static int SLEDGE_HAMMER   = 26;
+    public final static int HAMMER          = 27;
+    public final static int GOLF_CLUB       = 28;
+    public final static int STEAK_KNIFE     = 29;
+    public final static int TENNIS_RACKET   = 30;
+    
+    //ranged weapons
+    public final static int SHOTGUN         = 31;
+    public final static int HUNTING_RIFLE   = 32;
+    public final static int HANDGUN         = 33;
+    public final static int BOW_AND_ARROW   = 34;
+    
+    //random items
+    public final static int LAPTOP          = 35;
+    public final static int TEXTBOOK        = 36;
+    public final static int BOOTS           = 37;
+    public final static int STUFFED_ANIMALS = 38;
+    public final static int FRYING_PAN      = 39;
+    public final static int SOCCER_BALL     = 40;
+    public final static int BASKETBALL      = 41;
+    public final static int NOTEBOOK        = 42;
+    public final static int TV_REMOTE       = 43;
+    public final static int KEYS            = 44;
+    public final static int FISHING_ROD     = 45;
+    public final static int TENNIS_BALL     = 46;
+    
+    
+            
 }
