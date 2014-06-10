1ee092059a79433b99a85455ebed7481bfe2898f
Arturo Aguila <agu09001@byui.edu>
2014-06-10 16:40:52 -0600

    GameControl updated

--- SurvivRexburg/src/byui/cit260/survivRexburg/control/GameControl.java
+++ SurvivRexburg/src/byui/cit260/survivRexburg/control/GameControl.java
@@ -7,10 +7,14 @@
 package byui.cit260.survivRexburg.control;
 
 import Survivrexburg.SurviveRexburg;
+import byui.cit260.surviveRexburg.model.Backpack;
 import byui.cit260.surviveRexburg.model.EndUser;
 import byui.cit260.surviveRexburg.model.Game;
+import byui.cit260.surviveRexburg.model.GameCharacter;
+import byui.cit260.surviveRexburg.model.InventoryItems;
 import byui.cit260.surviveRexburg.model.Location;
 import byui.cit260.surviveRexburg.model.Map;
+import byui.cit260.surviveRexburg.model.Scenario;
 import static jdk.nashorn.internal.objects.NativeArray.map;
 import static jdk.nashorn.internal.objects.NativeDebug.map;
 
@@ -19,18 +23,12 @@ import static jdk.nashorn.internal.objects.NativeDebug.map;
  * @author arturoaguila
  */
 public class GameControl {
+    
     private static Game game;
     private int gameHungerValue;
     
-    /*
-    private listLocations(){
-        
-        for (var i=0, i<maxValue; i++){
-            
-        }
-    }*/
     
-    public static void startNewGame(){
+    public static void createNewGame(EndUser endUser){
         
         // create new game
         GameControl.game = new Game();
@@ -41,10 +39,105 @@ public class GameControl {
         //set player in game object
         GameControl.game.setEndUser(SurviveRexburg.getEndUser());
         
-        GameControl.createMap();    //creates game
-        GameControl.createEndUser();//creates game
+        //create list of characters that the EndUser can choose from
+        GameCharacter[] gameCharacters = GameControl.createCharList();
+        GameControl.game.setGameCharacters(gameCharacters);
+        
+        //create invetory list - it will be a list of possible items to be obtained
+        InventoryItems[] inventoryList = GameControl.createInventoryList();
+        GameControl.game.setInventoryItems(inventoryList);
+        
+        //create backpack and save in game
+        Backpack backpack = new Backpack();
+        GameControl.game.setBackpack(backpack);
+        
+        //creates map
+        Map map = GameControl.createMap();
+        GameControl.game.setMap(map);
+        
+        //move player to starting position
+        MapControl.moveActorToStartingLocation(0,3);
         
     }
+    
+    
+    //createNewGame functions
+    private static Map createMap() {
+        Map map = new Map(8,8);
+        /*
+        Scenario[] scenarios = createScenarios(); //create scenarios for location
+        
+        Location[] locations = new Location[64];
+        
+        Location gunStore gunStore = new Location("Gun Store", 1, 1, false);
+        
+        */
+        return map;
+    }
+
+    private static void createEndUser() {
+        System.out.println("***createEndUser function runs***");
+    }
+    
+    private static GameCharacter[] createCharList() {
+        GameCharacter[] gameCharacters = new GameCharacter[Constants.NUMBER_OF_CHARACTERS];
+        
+        GameCharacter ben = new GameCharacter(10, 5, 1, 4, "Ben");
+        gameCharacters[Constants.BEN] = ben;
+        
+        GameCharacter bing = new GameCharacter(6, 8, 3, 3, "Bing");
+        gameCharacters[Constants.BING] = bing;
+        
+        GameCharacter hayley = new GameCharacter(1, 5, 6, 8, "Hayley");
+        gameCharacters[Constants.HAYLEY] = hayley;
+        
+        GameCharacter megan = new GameCharacter(6, 2, 8, 4, "Megan");
+        gameCharacters[Constants.MEGAN] = megan;
+        
+        return gameCharacters;
+        
+    }
+    
+    
+    private static InventoryItems[] createInventoryList() {
+        InventoryItems[] inventoryItems = new InventoryItems[Constants.NUMBER_OF_ITEMS];
+    
+        InventoryItems cannedTuna = new InventoryItems(1, "Food","Canned Tuna");
+        inventoryItems[Constants.CANNED_TUNA] = cannedTuna;
+        
+        
+        
+        return inventoryItems;
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private static Scenario[] createScenarios() {
+        Scenario[] scenarios = new Scenario[64];
+        
+        Scenario nothing = new Scenario(100, "Nothing happens");
+        
+        
+        return Scenario;
+        
+        
+    }
+     
+     
+     
+     
+    
     
     public int increaseDailyDifficulty(int scenarioValue, int daysPassed) {
         
@@ -75,17 +168,6 @@ public class GameControl {
 
         return charHungerValue;
     }
-    
-    private static void createMap() {
-        Map map = new Map(20,20);
-        
-        System.out.println("***Choose Scenario function runs ***");
-    }
-
-    private static void createEndUser() {
-        System.out.println("***createEndUser function runs***");
-    }
-    
-    
    
+    
 }//end of class
