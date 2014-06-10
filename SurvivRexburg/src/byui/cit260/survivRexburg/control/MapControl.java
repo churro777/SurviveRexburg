3760ce71ad926b53fed828acdf812854165cd4fe
Arturo Aguila <agu09001@byui.edu>
2014-06-10 16:41:06 -0600

    MapControl updated

---
+++ SurvivRexburg/src/byui/cit260/survivRexburg/control/MapControl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.survivRexburg.control;
+
+/**
+ *
+ * @author arturoaguila
+ */
+class MapControl {
+
+    static void moveActorToStartingLocation(int i, int i0) {
+       System.out.println("***move actor to starting location function***");
+    }
+    
+}//end of class
