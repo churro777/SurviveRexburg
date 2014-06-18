5b55c0420de07be670c8d40c2f968e124fd2d01b
carissa888 <ada12025@byui.edu>
2014-06-18 16:18:48 -0600

    l

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/View.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arturoaguila
+ */
+public abstract class View implements ViewInterface{
+    
+    private String promptMessage;
+    
+    public View(String promptMessage) {
+        this.promptMessage = promptMessage;
+        
+        
+    }
+   
+    @Override
+    public void displayMenu() {
+        String value;
+        do {
+            
+            System.out.println(this.promptMessage);       // display the main menu
+            
+            value = this.getInput(); // get the user's selection
+            this.doAction(value);       // do action based on selection
+        } 
+        while (!value.equals("Q"));         // an selection is not "EXIT"
+    }
+
+    @Override
+    private String getInput() {
+        
+        Scanner keyboard = new Scanner(System.in); //keyboard input stream
+        boolean valid = false; //indicates if the name has been retrieved
+        String selection = null;
+        
+        
+        while (!valid) { //while a valid name ahs not been retrieved
+            
+            //get the name for the keyboard and trim off the blanks
+            System.out.println("\t\nEnter your selection below:");
+            
+            
+            selection = keyboard.nextLine();
+            selection = selection.trim();
+            
+            if (selection.length() < 1) { // exiting?
+                System.out.println("\n*** Invalid selection *** Try again");
+                continue;
+            }
+            
+            break;
+        }
+        return selection; //return the input
+    }
+
+    /*private void doAction(String choice) {
+        
+        switch (choice) {
+            case 'M': //display the movementdisplay
+                this.displayMap();
+                break;
+            case 'I': //show the current equipment display
+                this.viewInventory();
+                break;
+            case 'S':
+                this.displayScenarios();
+                break;
+            case 'Q': //exit menu and return to Main Menu
+                MainMenuView mainMenu = new MainMenuView();
+                mainMenu.displayMenu();
+                return;
+            default:    
+                System.out.println("\n**** Invalid selection *** Try again");
+                break;   
+        }
+    }*/
+    
+}//end of superclass
