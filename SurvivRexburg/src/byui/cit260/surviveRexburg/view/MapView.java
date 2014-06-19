9f265e02d5244d98f718bc5b5fee5d3650880594
carissa888 <ada12025@byui.edu>
2014-06-19 15:51:10 -0600

    mapView

--- SurvivRexburg/src/byui/cit260/surviveRexburg/view/MapView.java
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/MapView.java
@@ -13,7 +13,10 @@ import java.util.Scanner;
  * @author arturoaguila
  */
 public class MapView {
-    private final String MAP = "\n"
+    
+    public MapView {
+            
+        super("\n"
             + "\n------MAP-------"
             + "\n****************"
             + "\n|      |       |"
@@ -25,43 +28,11 @@ public class MapView {
             + "\n|      |       |"
             + "\n****************"
             + ""
-            + "Q - Quit to Game Menu";
-    
-    void displayMenu() {
-        char selection = ' ';
-        do{
-            
-            System.out.println(MAP);       // display the main menu
-            
-            String input = this.getInput(); // get the user's selection
-            selection = input.charAt(0);    //get first charcter of string
-            
-            this.doAction(selection);       // do action based on selection
-        } 
-        while (selection != 'Q');         // an selection is not "EXIT"
-    }
-
-    private String getInput() {
-        boolean valid = false; //indicates if the name has been retrieved
-        String input = null;
-        Scanner keyboard = new Scanner(System.in); //keyboard input stream
+            + "Q - Quit to Game Menu");
         
-        while(!valid) { //while a valid name ahs not been retrieved
-            
-            //get the name for the keyboard and trim off the blanks
-            input = keyboard.nextLine();
-            input = input.trim();
-            
-            if (input.toUpperCase().equals("Q")) { // exiting?
-                return null;
-            }
-            
-                valid = true; //signal that a valid name was entered
-        }
-        return input; //return the input
-    }
+    }    
 
-    private void doAction(char selection) {
+    private void doAction(String value) {
          System.out.println("\n"
                  + "\n **** Open GameplayScreen *****");
     }
