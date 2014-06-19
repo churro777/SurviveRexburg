34ac6e7802ad38e66dea3ef99540279546d3544c
carissa888 <ada12025@byui.edu>
2014-06-19 16:22:22 -0600

    NewGameScreenView

--- SurvivRexburg/src/byui/cit260/surviveRexburg/view/NewGameScreenView.java
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/view/NewGameScreenView.java
@@ -16,70 +16,34 @@ import java.util.Scanner;
 /**
  *
  * @author arturoaguila
+ * 
+ * 
+ * 
+ * 
  */
-public class NewGameScreenView {
-    
-    public void NewGameScreen() {
-        
-        System.out.println(NEWGAME);
-        
-        //prompt the player to enter their name Retrieve the name of player
-        String playersName = this.getPlayersName();
+public class NewGameScreenView extends View{
+   
+    public NewGameScreenView(){
+    super("\n"
+        + "\n----------------NEW GAME------------------"
+        + "\n| Enter the name of your game file.      |"
+        + "\n------------------------------------------");
+    }
+
+    @Override
+    public void doAction(String value){
         
         //Create the player object and save it in the ProgramControl class
-        EndUser player = ProgramControl.createPlayer(playersName);
+        EndUser player = ProgramControl.createPlayer(value);
         SurviveRexburg.setEndUser(player);
         
-/*<<<<<<< HEAD
-=======
-        
-
-        
->>>>>>> origin/master */
         //Display the Main Menu
         MainMenuView mainMenuView = new MainMenuView();
         mainMenuView.display();
-
+        
+        
+        
     }
-    
-            private final String NEWGAME ="\n"
-            + "\n----------------NEW GAME------------------"
-            + "\n| Enter the name of your game file.      |"
-            + "\n------------------------------------------";
-    
-    
-    
-    
-    private String getPlayersName() {
-        boolean valid = false; //indicates if the name has been retrieved
-        String playersName = null;
-        Scanner keyboard = new Scanner(System.in); //keyboard input stream
-        
-        while(!valid) { //while a valid name ahs not been retrieved
-            
-            //get the name for the keyboard and trim off the blanks
-            playersName = keyboard.nextLine();
-            playersName = playersName.trim();
-            
-            if (playersName.toUpperCase().equals("Q")) { // Quiting?
-                return null;
-            } 
-            
-            //if the name is invalid (non-blank and < two characters)
-            if (playersName.length() < 2) {
-                //display and error
-                System.out.println("Invalid name - the name must be non blank "
-                                    + "greater than the one character in length");
-            }
-            else {
-                valid = true; //signal that a valid name was entered
-            }
-           
-        }
-        
-        return playersName; //return the name
-    } 
-  
- 
+
     
 }//end of NewGameScreenView class
