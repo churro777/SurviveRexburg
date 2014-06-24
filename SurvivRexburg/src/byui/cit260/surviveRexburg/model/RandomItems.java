db90a77c8c0d7a15fee7282f78f1bfc37c8f15a8
carissa888 <ada12025@byui.edu>
2014-06-24 16:19:45 -0600

    meleeweapons REALLY mad

---
+++ SurvivRexburg/src/byui/cit260/surviveRexburg/model/RandomItems.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.surviveRexburg.model;
+
+import byui.cit260.surviveRexburg.model.InventoryItems;
+import java.io.Serializable;
+
+/**
+ *
+ * @author carissa888
+ */
+class RandomItems extends InventoryItems implements Serializable{
+    
+    
+    
+}
